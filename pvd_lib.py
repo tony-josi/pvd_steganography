29828651f61e30cb9f4c73d703bbaf314bfd3f44
Tony Josi <tonyjosinew@gmail.com>
2022-06-25 13:44:21 +0530

    main moved to test_main.py

--- pvd_lib.py
+++ pvd_lib.py
@@ -320,12 +320,3 @@ class pvd_lib:
 
 
 
-""" Test """
-if __name__ == "__main__":
-
-    pvd_obj = pvd_lib()
-
-    if sys.argv[1] == 'e' or sys.argv[1] == 'E':
-        pvd_obj.pvd_embed(sys.argv[2], sys.argv[3], sys.argv[4])
-    elif sys.argv[1] =='d' or sys.argv[1] == 'D':
-        pvd_obj.pvd_extract(sys.argv[2], sys.argv[3], sys.argv[4])
--- test_main.py
+++ test_main.py
@@ -1,7 +1,13 @@
 import sys
-from PIL import Image
 
-img = Image.open(sys.argv[1])
-pix = img.load()
-lent, hig = img.size
-print(lent, hig)
+from pvd_lib import pvd_lib
+
+""" Test """
+if __name__ == "__main__":
+
+    pvd_obj = pvd_lib()
+
+    if sys.argv[1] == 'e' or sys.argv[1] == 'E':
+        pvd_obj.pvd_embed(sys.argv[2], sys.argv[3], sys.argv[4])
+    elif sys.argv[1] =='d' or sys.argv[1] == 'D':
+        pvd_obj.pvd_extract(sys.argv[2], sys.argv[3], sys.argv[4])
