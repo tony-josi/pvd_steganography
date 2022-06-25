39ee2648d4fb00d72035418b76e4c7868a528caa
Tony Josi <tonyjosinew@gmail.com>
2022-06-25 15:35:44 +0530

    Adding more comments

--- pvd_lib.py
+++ pvd_lib.py
@@ -24,6 +24,16 @@ PVD_BYTES_TO_BITS           = 8
 
 PVD_BYTE_ORDER              = 'big'
 
+"""
+This class implements the uitility functionality for reading
+required amount of bits from a given input file for the purpose of encoding.
+
+The constructor reads the entire file to RAM and feeds the required amount
+bits when get_bits() is called.
+
+The class also handles the header information required to identify the 
+PVD library magic sequence, PVD library version and encoded data size.
+"""
 class file_bits_reader:
     
     data = None
@@ -34,10 +44,18 @@ class file_bits_reader:
     def __init__(self, f_path):
         try:
             self.f_obj = open(f_path, "rb")
+
+            """ Read the entire file """
             self.data = list(self.f_obj.read())
             data_len = len(self.data)
+            
+            """ Add header info of PVD library to the beginning
+            of the data. """
             self.data = PVD_MAGIC + PVD_VERSION + list(data_len.to_bytes(PVD_MAX_LENGTH_FIELD, PVD_BYTE_ORDER)) + self.data
             self.total_bytes = len(self.data) 
+
+            """ Start giving required amount of bits by converting each byte (starting by 1st)
+            to a binary string which will be easier to splice. """
             self.byte_read = format(self.data[0], '#010b')[2:]
             self.bits_read_in_cur_byte = 0
             self.bytes_read_so_far += 1
@@ -47,6 +65,8 @@ class file_bits_reader:
             print("ERROR: Opening file: {} EXCP: {}".format(f_path, e))
 
     def get_bits(self, bits):
+
+        """ Can only give bits between 1 to 8 """
         if bits > 8 or bits <= 0:
             raise ValueError("Bits should be between 0 and 8 bits")
 
@@ -67,11 +87,15 @@ class file_bits_reader:
             ret_val = int(self.byte_read[self.bits_read_in_cur_byte:(self.bits_read_in_cur_byte + remaining)], 2)
             self.bits_read_in_cur_byte += remaining
 
+        """ If the current byte is processed completely? """
         if self.bits_read_in_cur_byte == 8 and self.bytes_read_so_far < self.total_bytes:
+            """ Read next byte and convert it to binary string """
             self.byte_read = format(self.data[self.bytes_read_so_far], '#010b')[2:]
             self.bytes_read_so_far += 1
             self.bits_read_in_cur_byte = 0
 
+        """ If the required bits count was not covered using the previous byte
+        get bits from the newly read byte. """
         if self.bits_read_in_cur_byte < 8 and bits - remaining > 0:
             read_end = bits - remaining
             ret_val <<= (bits - remaining)
@@ -87,6 +111,16 @@ class file_bits_reader:
         if self.f_obj:
             self.f_obj.close()
 
+"""
+This class implements the uitility functionality for writing
+given data bits to a given output file for the purpose of decoding back.
+
+The close file function writes the buffer to the file.
+
+The class also handles the header information required to identify the 
+PVD library magic sequence, PVD library version and encoded data size. And avoids
+writing that data to the output file.
+"""
 class file_bits_writer:
     def __init__(self, f_path):
         try:
@@ -108,6 +142,9 @@ class file_bits_writer:
             self.cur_byte <<= bits
             self.cur_byte |= data
             self.bits_wrote_in_cur_byte += bits
+
+            """ If the incoming bits and previously available bits can form more than
+            a byte worth of data? """
         else:
             remaining_reqd = 8 - self.bits_wrote_in_cur_byte
             bits_str = bin(data)[2:].zfill(bits)
@@ -115,14 +152,19 @@ class file_bits_writer:
                 self.cur_byte <<= remaining_reqd
                 self.cur_byte |= int(bits_str[:remaining_reqd], 2)
                 self.bits_wrote_in_cur_byte += remaining_reqd
+
+            """ Append the byte to the buffer """
             self.data.append(self.cur_byte)
             self.cur_byte = 0
             self.bits_wrote_in_cur_byte = 0
             self.bytes_wrote_to_file_so_far += 1
             self.bits_wrote_in_cur_byte += (bits - remaining_reqd)
+
+            """ Process the remaining bits to the newly started byte. """
             self.cur_byte <<= (bits - remaining_reqd)
             self.cur_byte |= int(bits_str[remaining_reqd:], 2) 
 
+        """ If end of file (eof) reached close it """
         if is_eof:
             self.data.append(self.cur_byte)
             self.bytes_wrote_to_file_so_far += 1
@@ -135,11 +177,15 @@ class file_bits_writer:
             self.f_obj.close()
 
 
+""" The PVD library. """
 class pvd_lib:
 
     def __init__(self):
         pass
 
+    """ The PVD table that converts the pixel difference to the
+    number of bits that can be used from the Least Significant bits to replace
+    with the secret data. """
     @staticmethod
     def _pvd_table(p_diff):
         nbits = 0
@@ -151,6 +197,8 @@ class pvd_lib:
             nbits = 4
         return nbits
 
+    """ Calculated the embedding capacity of a given 
+    image. """
     @staticmethod
     def _embed_capacity(ref_image_path):
 
@@ -167,12 +215,19 @@ class pvd_lib:
             if no_of_matrix_h < 1 or no_of_matrix_w < 1 or len(pixels[0, 0]) < 3:
                 return embed_capacity;
 
+            """ Split the image to [3 x 3] blocks """
             for height_itr in range(0, no_of_matrix_h * 3, 3):
                 for width_itr in range(0, no_of_matrix_w * 3, 3):
 
                     #print(pixels[width_itr + 1, height_itr + 1])
+                    """ Get the middle pixel as reference pixel """
                     ref_rgb = pixels[height_itr + 1, width_itr + 1]
 
+                    """ Iterate through the remaining pixels in the order
+                    [1 2 3]
+                    [4 - 5]
+                    [6 7 8]
+                    """
                     for h_j in range(height_itr, height_itr + 3):
                         for w_i in range(width_itr, width_itr + 3):
 
@@ -188,12 +243,14 @@ class pvd_lib:
         #print(embed_capacity // 8)
         return embed_capacity // 8
 
+    """ Replace the given LS bits with given data """
     @staticmethod
     def replace_lsbs(pixel, bits, value):
         mask = (1 << bits) - 1
         pixel &= (~mask)
         return (pixel | value)
 
+    """ Get the given number of LS bits data """
     @staticmethod
     def get_lsbs(pixel, bits):
         mask = (1 << bits) - 1
@@ -238,15 +295,23 @@ class pvd_lib:
                             for rgb in range(3):
                                 bits_reqd = pvd_lib._pvd_table(abs(c_rgb[rgb] - ref_rgb[rgb]))
                                 embedded_ds += bits_reqd
+
+                                """ Get the required number of bits corresponding to
+                                the pixel difference from the current pixel and reference pixel from
+                                the input file. """
                                 ret_val = bits_reader.get_bits(bits_reqd)
+
+                                """Replace the LSBs of the pixel with the file data. """
                                 c_rgb_list[rgb] = pvd_lib.replace_lsbs(c_rgb[rgb], ret_val[2], ret_val[1])
                                 if ret_val[0] == True:
                                     done_embedding = True
                                     break
 
+                            """ Replace the pixel value with modified contents """
                             pixels[h_j, w_i] = tuple(c_rgb_list)
 
                             if done_embedding:
+                                """ Write the output image """
                                 img_obj.save(op_img_path)
                                 bits_reader.close_file()
                                 return embedded_ds
@@ -299,17 +364,23 @@ class pvd_lib:
                                 ret_val = bits_writer.set_bits(eof_reached, bits_reqd, data)
                                 if magic_extracted and (encoded_size + PVD_HEADER_SIZE) == bits_writer.bytes_wrote_to_file_so_far:
                                     eof_reached = True
+
+                                """ If we have completed reading the header? """
                                 if (bits_writer.bytes_wrote_to_file_so_far >= (PVD_HEADER_SIZE)) and magic_extracted == False:
                                     magic_extracted = True
                                     magic = bits_writer.data[:PVD_HEADER_SIZE]
                                     pvd_magic = magic[:4]
                                     pvd_versn = magic[4:7]
+                                    """ Check if the magic and version are matching """
                                     if pvd_magic != PVD_MAGIC or pvd_versn != PVD_VERSION:
                                         raise ValueError("Invalid version or image... magic: {} versn: {}".format(pvd_magic, pvd_versn))
                                     size_arr = magic[-4:]
+                                    """ Parse the encoded data size in the image """
                                     encoded_size = (size_arr[0] << 24) + (size_arr[1] << 16) + (size_arr[2] << 8) + (size_arr[3] << 0)
                                 
                                 if eof_reached:
+
+                                    """ If we have read all the data required then close the file """
                                     bits_writer.close_file()
                                     return embedded_ds
 
@@ -317,6 +388,7 @@ class pvd_lib:
 
 
 
+    """ Wrapper for embedding """
     def pvd_embed(self, ref_image_path, secret_file_path, op_img_path):
         
         embed_cap = pvd_lib._embed_capacity(ref_image_path)
@@ -328,9 +400,7 @@ class pvd_lib:
 
         return self.embed_data(ref_image_path, secret_file_path, op_img_path)
 
+    """ Wrapper for extracting """
     def pvd_extract(self, ref_image_path, secret_op_file, pvd_img_path):
 
         return self.extract_data(ref_image_path, secret_op_file, pvd_img_path)
-
-
-
